56c3d535f54c271e2f8e1e51941b6c2fd92d319f
Yurii Antonchyk <xsat.php@gmail.com>
2023-10-09 23:33:36 +0300

    Implement win logic

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -25,17 +25,16 @@ func _ready() -> void:
 		tail_variant.text = _generate_random_symbol()
 		
 	tails = [
-		$Tails/Tail1, $Tails/Tail2, $Tails/Tail3, 
-		$Tails/Tail4, $Tails/Tail5, $Tails/Tail6, 
-		$Tails/Tail7, $Tails/Tail8, $Tails/Tail9,
+		$Tails/Tail0, $Tails/Tail1, $Tails/Tail2, 
+		$Tails/Tail3, $Tails/Tail4, $Tails/Tail5, 
+		$Tails/Tail6, $Tails/Tail7, $Tails/Tail8,
 	]
 	
 func _on_tail_variant_select(tail_variant: TailVariant) -> void:
 	if !tails[next_tail_position].visible:
 		tails[next_tail_position].visible = true
 	else:
-		game_score -= 1
-		score.text = "%s" % game_score
+		_add_to_game_score(-1)
 		
 	tails[next_tail_position].symbol.text = tail_variant.text
 	
@@ -46,5 +45,67 @@ func _on_tail_variant_select(tail_variant: TailVariant) -> void:
 	
 	tail_variant.text = _generate_random_symbol()
 	
+	while _is_delete_victory_tails():
+		_add_to_game_score(3)
+	
 func _generate_random_symbol() -> String:
 	return tail_symbols.pick_random()
+	
+func _is_delete_victory_tails() -> bool:
+	if _is_same_values(tails[0], tails[1], tails[2]):
+		_remove_victory_tails(tails[0], tails[1], tails[2])
+		
+		return true
+		
+	if _is_same_values(tails[3], tails[4], tails[5]):
+		_remove_victory_tails(tails[3], tails[4], tails[5])
+		
+		return true
+		
+	if _is_same_values(tails[6], tails[7], tails[8]):
+		_remove_victory_tails(tails[6], tails[7], tails[8])
+		
+		return true
+		
+	if _is_same_values(tails[0], tails[3], tails[6]):
+		_remove_victory_tails(tails[0], tails[3], tails[6])
+		
+		return true
+		
+	if _is_same_values(tails[1], tails[4], tails[7]):
+		_remove_victory_tails(tails[1], tails[4], tails[7])
+		
+		return true
+		
+	if _is_same_values(tails[2], tails[5], tails[8]):
+		_remove_victory_tails(tails[2], tails[5], tails[8])
+		
+		return true
+		
+	if _is_same_values(tails[0], tails[4], tails[8]):
+		_remove_victory_tails(tails[0], tails[4], tails[8])
+		
+		return true
+		
+	if _is_same_values(tails[6], tails[4], tails[2]):
+		_remove_victory_tails(tails[6], tails[4], tails[2])
+		
+		return true
+		
+	return false
+		
+func _is_same_values(first_tail: Tail, second_tail: Tail, third_tail: Tail) -> bool:
+	if !first_tail.visible || !second_tail.visible || !third_tail.visible:
+		return false
+	
+	return first_tail.symbol.text == second_tail.symbol.text && second_tail.symbol.text == third_tail.symbol.text 
+	
+func _add_to_game_score(added_value: int) -> void:
+	game_score += added_value
+	score.text = "%s" % game_score
+	
+func _remove_victory_tails(first_tail: Tail, second_tail: Tail, third_tail: Tail) -> void:
+	first_tail.visible = false
+	second_tail.visible = false
+	third_tail.visible = false
+	
